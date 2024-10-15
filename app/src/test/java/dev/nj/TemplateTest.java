711c979c62e5e6051e1cd622c9e8852bffb27ec8
eucarizan <eucarizan@gmail.com>
2024-10-15 16:40:27 +0800

    test: multiple variables

--- app/src/test/java/dev/nj/TemplateTest.java
+++ app/src/test/java/dev/nj/TemplateTest.java
@@ -25,4 +25,13 @@ class TemplateTest {
         template.set("name", "someone else");
         assertEquals("Hi, someone else", template.evaluate());
     }
+
+    @Test
+    public void multipleVariables() throws Exception {
+        Template template = new Template("${one}, ${two}, ${three}");
+        template.set("one", "1");
+        template.set("two", "2");
+        template.set("three", "3");
+        assertEquals("1 2 3", template.evaluate());
+    }
 }
